73daa3529c3e5fe802370d127f4ab0f6e8c4a256
04stefke <jovanovics399@yahoo.com>
2024-12-14 22:25:22 +0000

    created main.js file

---
+++ main.js
@@ -0,0 +1,6 @@
+const keys = document.getElementById("key");
+const displayOutput = document.getElementById("displayOutput");
+const displayInput = document.getElementById("displayInput");
+
+let input = "";
+
