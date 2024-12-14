3c0943289039b83748e63bb29deba83d2117f274
04stefke <jovanovics399@yahoo.com>
2024-12-14 22:42:27 +0000

    created the for loop for checking  the key and event listener for the key which checks what btn is clicked and does the supposed action

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-const keys = document.getElementById("key");
+const keys = document.querySelectorAll("#key");
 const displayOutput = document.getElementById("displayOutput");
 const displayInput = document.getElementById("displayInput");
 
 let input = "";
 
+for (let key of keys) {
+	const value = key.value;
+	key.addEventListener("click", () => {
+		if (value == "reset") {
+			input = "";
+			displayOutput.textContent = "";
+			displayInput.textContent = "";
+		} else if (value == "del") {
+			input = input.slice(0, -1);
+			displayOutput.textContent = input;
+            displayInput.textContent = '';
+		} else if (value == "=") {
+			let result = eval(input);
+            console.log(input);
+			displayInput.textContent = result;
+		} else {
+			input += value;
+			displayOutput.textContent = input;
+		}
+	});
+}
